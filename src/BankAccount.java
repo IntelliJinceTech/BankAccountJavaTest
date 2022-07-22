8217d6039bb190aeb1ee29763a79769a974d650a
Christopher Jin <chrisjin1@gmail.com>
2022-07-22 02:06:32 -0400

    playing around with java classes

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,6 +1,17 @@
 import com.sun.jdi.connect.Connector;
 
 public class BankAccount {
+    //instance fields
+    String name;
+    double balance;
+    int yearsOpen;
+
+    //constructor method
+    public BankAccount(String accountOwner, double accountBalance, int accountYears ) {
+        name = accountOwner;
+        balance = accountBalance;
+        yearsOpen = accountYears;
+    }
 
     // Show balance
     // Deposit Money
@@ -10,6 +21,7 @@ public class BankAccount {
 
     public static void main(String[] Args) {
         System.out.println("Does this work?");
-
+        BankAccount PNC = new BankAccount("Christopher Jin", 200000.00, 10);
+        System.out.println(PNC.name);
     }
 }
